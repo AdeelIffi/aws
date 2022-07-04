31dcf94d952ec7c7ed8cf17616d27a7d62375fa2
AhmedAZafar <35378345+AhmedAZafar@users.noreply.github.com>
2022-07-04 20:01:13 +0100

    update
    subnet and commented out ec2

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -25,3 +25,17 @@ resource "aws_vpc" "my-vpc" {
 resource "aws_internet_gateway" "my-vpc-igw" {
     vpc_id = aws_vpc.my-vpc.id
 }
+
+resource "aws_subnet" "my-vpc-subnet" {
+
+    vpc_id = aws_vpc.my-vpc.id
+    cidr_block = "10.11.1.0/24"
+}
+
+# resource "aws_instance" "my-vpc-ec2-instance" {
+
+#     ami = "ami-0a5318a691549cd2b"
+
+#     instance_type = "t3.micro"
+  
+# }
